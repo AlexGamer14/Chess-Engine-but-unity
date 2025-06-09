e45f1d49d14ce2212490c486e61b952d66d9d344
AlexTheEpic <99535276+AlexGamer14@users.noreply.github.com>
2025-06-09 21:39:35 +0200

    Fix👌

--- Assets/Core/ChessBot/ChessEngine.cs
+++ Assets/Core/ChessBot/ChessEngine.cs
@@ -180,7 +180,7 @@ namespace ChessEngine
         public void LoadFenString()
         {
             // rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
-            string fenString = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+            string fenString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
             board = new();
 
 
